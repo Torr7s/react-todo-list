738831a0db1cc552af5a0c2ce7e327608b9d7b36
Torr7s <torresmiranda.rj@gmail.com>
2022-06-06 10:32:38 -0300

    Create Form component

---
+++ src/components/Form/index.js
@@ -0,0 +1,31 @@
+import './Form.css';
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { FaPlus } from 'react-icons/fa';
+
+export default function Form({ handleSubmit, handleChange, newTask }) {
+  return (
+    <form
+      action="#"
+      className="form"
+      onSubmit={handleSubmit}
+    >
+      <input
+        onChange={handleChange}
+        type="text"
+        value={newTask}
+      />
+      <button type="submit">
+        <FaPlus />
+      </button>
+    </form>
+  );
+}
+
+Form.propTypes = {
+  handleChange: PropTypes.func.isRequired,
+  handleSubmit: PropTypes.func.isRequired,
+  newTask: PropTypes.string.isRequired
+}
